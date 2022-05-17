b5da612b1dcc85b3d0825fce734d51107dba97f2
andres <aguilar_andres@protonmail.com>
2022-05-16 21:23:30 -0700

    stored lenght of numbers array in variable

--- progress_bar.py
+++ progress_bar.py
@@ -10,7 +10,8 @@ numbers = [ x * 5 for x in range(2000, 3000) ]
 results = []
 
 progress_bar( 0, len(numbers) )
+length = len(numbers)
 
 for i, x in enumerate(numbers):
   results.append( math.factorial(x) )
-  progress_bar(i + 1, len(numbers))
+  progress_bar(i + 1, length)
